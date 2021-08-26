122d46b8cf8d9cdd88ead27a9b8d5cb88bfba0c6
dharasubhamay <82009535+dharasubhamay@users.noreply.github.com>
2021-08-26 19:03:43 +0530

    Update main.py

--- Day-1/Exercise 1.1/main.py
+++ Day-1/Exercise 1.1/main.py
@@ -1,4 +1,6 @@
 """
+## Printing to the Console
+
 # Instructions
 
 Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function. 
