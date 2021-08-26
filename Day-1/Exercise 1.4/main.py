1637be8a50dd8e9a500c2a301183048bd0054bf0
dharasubhamay <82009535+dharasubhamay@users.noreply.github.com>
2021-08-26 19:07:28 +0530

    Update main.py

--- Day-1/Exercise 1.4/main.py
+++ Day-1/Exercise 1.4/main.py
@@ -1,19 +1,40 @@
-# 🚨 Don't change the code below 👇
+"""
+## Variables
+
+# Instructions
+
+Write a program that switches the values stored in the variables a and b. 
+
+**Warning.** Do not change the code on lines 1-4 and 12-18. Your program should work for different inputs. e.g. any value of a and b.
+
+# Example Input
+
+a: 3
+b: 5
+
+# Example Output
+
+a: 5
+b: 3
+"""
+
+
+# Don't change the code below
 a = input("a: ")
 b = input("b: ")
-# 🚨 Don't change the code above 👆
+# Don't change the code above
 
 ####################################
-#Write your code below this line 👇
+#Write your code below this line
 
 temp = a
 a = b
 b = temp
 
-#Write your code above this line 👆
+#Write your code above this line
 ####################################
 
-# 🚨 Don't change the code below 👇
+# Don't change the code below
 print("a: " + a)
 print("b: " + b)
 
